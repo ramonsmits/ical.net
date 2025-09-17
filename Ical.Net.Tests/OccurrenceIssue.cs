c81b916c43a9e3eaaec7200fbe9dfec08269d35a
Ramon Smits <ramon.smits@gmail.com>
2025-09-17 17:09:07 +0200

    Ported from `occurrence-issue-v4`

--- Ical.Net.Tests/OccurrenceIssue.cs
+++ Ical.Net.Tests/OccurrenceIssue.cs
@@ -14,59 +14,55 @@ public class OccurrenceIssue
     {
         var cal = Calendar.Load("""
             BEGIN:VCALENDAR
-            PRODID:-//Google Inc//Google Calendar 70.9054//EN
             VERSION:2.0
-            CALSCALE:GREGORIAN
-            METHOD:PUBLISH
-            X-WR-CALNAME:Non-Critical Support Roster
-            X-WR-TIMEZONE:UTC
+            PRODID:-//Test//EN
             BEGIN:VEVENT
             DTSTART;VALUE=DATE:20251103
             DTEND;VALUE=DATE:20251124
             RRULE:FREQ=WEEKLY;WKST=MO;INTERVAL=48;BYDAY=MO
-            DTSTAMP:20250912T100327Z
-            UID:00p0ja7t2446ja22sl02ah2uun@google.com
-            CREATED:20250214T230308Z
-            LAST-MODIFIED:20250409T173619Z
-            SEQUENCE:1
-            STATUS:CONFIRMED
-            SUMMARY:QWERTY
-            TRANSP:TRANSPARENT
+            UID:test-uid@example.com
+            SUMMARY:Master Event
             END:VEVENT
             BEGIN:VEVENT
             DTSTART;VALUE=DATE:20251103
             DTEND;VALUE=DATE:20251124
-            DTSTAMP:20250912T100327Z
-            UID:00p0ja7t2446ja22sl02ah2uun@google.com
+            UID:test-uid@example.com
             RECURRENCE-ID;VALUE=DATE:20251103
-            CREATED:20250214T230308Z
-            LAST-MODIFIED:20250428T170953Z
             SEQUENCE:1
-            STATUS:CONFIRMED
-            SUMMARY:QWERTY
-            TRANSP:TRANSPARENT
+            SUMMARY:Override Event
             END:VEVENT
             END:VCALENDAR
             """)!;
 
-        Console.WriteLine("Events:");
-        foreach (var e in cal.Events.OrderBy(x=>x.Start))
-        {
-            Console.WriteLine($"\t{e.Uid.Substring(0,7)} {e.Start.Value} {e.End.Value} {e.Summary}");
-        }
-
-        var dt = new CalDateTime(DateOnly.FromDateTime(new DateTime(2026,1,1)));
+        var dt = new CalDateTime(DateOnly.FromDateTime(new DateTime(2027, 1, 1)));
         var occurrences = cal
             .GetOccurrences<CalendarEvent>()
-            .TakeWhile(p => p.Period.StartTime <= dt);
+            .TakeWhile(p => p.Period.StartTime <= dt)
+            .OrderBy(x => x.Period.StartTime)
+            .ToList();
+
+        Console.WriteLine("Events:");
+        foreach (var e in cal.Events.OrderBy(x => x.Start))
+        {
+            Console.WriteLine($"\t{e.Uid} {e.Start.Value}"); // {e.End.Value}
+        }
 
         Console.WriteLine("Occurrences:");
         foreach (var o in occurrences)
         {
-            var e = (CalendarEvent)o.Source;
-            Console.WriteLine($"\t{e.Uid.Substring(0,7)} {o.Period.StartTime.Value} {o.Period.EffectiveEndTime.Value} {e.Summary}");
+            var e = (CalendarEvent) o.Source;
+            Console.WriteLine($"\t{e.Uid} {o.Period.StartTime.Value} {o.Period.EffectiveEndTime.Value} {e.Summary}");
         }
 
-        Assert.That(occurrences, Is.Not.Empty);
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(occurrences, Has.Count.EqualTo(2));
+            Assert.That(occurrences[0].Period.StartTime, Is.EqualTo(new CalDateTime(2025, 11,3)));
+            Assert.That(occurrences[0].Source.RecurrenceId, Is.EqualTo(new CalDateTime(2025, 11, 3)));
+            Assert.That(occurrences[1].Period.StartTime, Is.EqualTo(new CalDateTime(2026, 10, 5)));
+            // Not sure why the following is null, in v5 it is not null
+            Assert.That(occurrences[1].Source.RecurrenceId, Is.Null);
+            Assert.That(((CalendarEvent) occurrences[0].Source).Summary, Is.EqualTo("Override Event"));
+        }
     }
 }
